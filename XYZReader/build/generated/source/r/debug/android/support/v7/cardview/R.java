c339ce79ad0960fe5d9280d0808c97b7042eae6d
rashikaranpuria <2013165@iiitdmj.ac.in>
2017-02-16 13:52:09 +0530

    fix: image scale types

--- XYZReader/build/generated/source/r/debug/android/support/v7/cardview/R.java
+++ XYZReader/build/generated/source/r/debug/android/support/v7/cardview/R.java
@@ -21,10 +21,10 @@ public final class R {
 		public static final int contentPaddingTop = 0x7f010028;
 	}
 	public static final class color {
-		public static final int cardview_dark_background = 0x7f0c0012;
-		public static final int cardview_light_background = 0x7f0c0013;
-		public static final int cardview_shadow_end_color = 0x7f0c0014;
-		public static final int cardview_shadow_start_color = 0x7f0c0015;
+		public static final int cardview_dark_background = 0x7f0c0013;
+		public static final int cardview_light_background = 0x7f0c0014;
+		public static final int cardview_shadow_end_color = 0x7f0c0015;
+		public static final int cardview_shadow_start_color = 0x7f0c0016;
 	}
 	public static final class dimen {
 		public static final int cardview_compat_inset_shadow = 0x7f09003a;
